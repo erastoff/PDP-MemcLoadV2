73424c3fd7a6c4f9c0cf66ffaec1dbeea9e564e9
erastoff <yuriy.erastov@yandex.ru>
2024-05-21 20:42:25 +0300

    Master - protofile generated / some function added in memc_load.go

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module PDP-MemcLoadV2
 go 1.22
 
 require github.com/bradfitz/gomemcache v0.0.0-20230905024940-24af94b03874
+
+require (
+	github.com/golang/protobuf v1.5.4 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
--- memc_load.go
+++ memc_load.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"PDP-MemcLoadV2/appsinstalled"
+	"bufio"
+	"compress/gzip"
 	"flag"
+	"fmt"
 	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/golang/protobuf/proto"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
-var DEV_MEMC_CON = map[string]MemcacheStorage{}
+var DEV_MEMC_CON = make(map[string]*MemcacheStorage)
+var normalErrRate = 0.01
 
 type MemcacheStorage struct {
 	Client *memcache.Client
 }
 
 func NewMemcacheStorage(addr string) *MemcacheStorage {
-	mc := memcache.New(addr)
-	return &MemcacheStorage{Client: mc}
+	storage, exists := DEV_MEMC_CON[addr]
+	if !exists {
+		// Если не существует, создаем новый клиент и сохраняем его в мапе
+		client := memcache.New(addr)
+		storage = &MemcacheStorage{Client: client}
+		DEV_MEMC_CON[addr] = storage
+	}
+	return storage
+	//mc := memcache.New(addr)
+	//return &MemcacheStorage{Client: mc}
+
 }
 
 type AppsInstalled struct {
@@ -41,7 +58,8 @@ func main() {
 	flag.BoolVar(&test, "test", false, "Run tests")
 	flag.StringVar(&logFile, "log", "", "Log file path")
 	flag.BoolVar(&dryRun, "dry", false, "Dry run mode")
-	flag.StringVar(&pattern, "pattern", "/data/appsinstalled/*.tsv.gz", "File pattern")
+	flag.StringVar(&pattern, "pattern", "data/appsinstalled/*.tsv.gz", "File pattern")
+	//flag.StringVar(&pattern, "pattern", "*.go", "File pattern")
 	flag.StringVar(&idfa, "idfa", "127.0.0.1:33013", "IDFA memcached address")
 	flag.StringVar(&gaid, "gaid", "127.0.0.1:33014", "GAID memcached address")
 	flag.StringVar(&adid, "adid", "127.0.0.1:33015", "ADID memcached address")
@@ -64,13 +82,140 @@ func main() {
 		"adid": adid,
 		"dvid": dvid,
 	}
-
+	fmt.Printf("File list creating in pattert '%s'\n...\n", pattern)
 	fileList, err := filepath.Glob(pattern)
 	if err != nil {
 		log.Fatalf("Error finding files: %v", err)
 	}
+	for _, filename := range fileList {
+		processFile(filename, deviceMemc, dryRun)
+	}
+}
 
-	//for _, filename := range fileList {
-	//	processFile(filename, deviceMemc, dryRun)
-	//}
+func processFile(filePath string, deviceMemc map[string]string, dryRun bool) {
+	var processed, errors int
+	//var errors int
+	log.Printf("Processing %s", filePath)
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Printf("Error opening file: %v", err)
+		return
+	}
+	defer file.Close()
+
+	gzReader, err := gzip.NewReader(file)
+	if err != nil {
+		log.Printf("Error creating gzip reader: %v", err)
+		return
+	}
+	defer gzReader.Close()
+
+	scanner := bufio.NewScanner(gzReader)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		appsinstalled := parseAppsinstalled(line)
+		fmt.Println(appsinstalled)
+		if appsinstalled == nil {
+			errors++
+			continue
+		}
+		memcAddr, ok := deviceMemc[appsinstalled.DevType]
+		//_, ok := deviceMemc[appsinstalled.DevType]
+		if !ok {
+			errors++
+			log.Printf("Unknown device type: %s", appsinstalled.DevType)
+			continue
+		}
+		ok = insertAppsinstalled(memcAddr, *appsinstalled, dryRun)
+		if ok {
+			processed++
+		} else {
+			errors++
+		}
+	}
+
+	if processed == 0 {
+		dotRename(filePath)
+		return
+	}
+	errRate := float64(errors) / float64(processed)
+	if errRate < normalErrRate {
+		log.Printf("Acceptable error rate (%.2f). Successful load", errRate)
+	} else {
+		log.Printf("High error rate (%.2f > %.2f). Failed load", errRate, normalErrRate)
+	}
+	dotRename(filePath)
+}
+
+func parseAppsinstalled(line string) *AppsInstalled {
+	lineParts := strings.Split(strings.TrimSpace(line), "\t")
+	if len(lineParts) < 5 {
+		return nil
+	}
+	devType, devID, latStr, lonStr, rawApps := lineParts[0], lineParts[1], lineParts[2], lineParts[3], lineParts[4]
+	if devType == "" || devID == "" {
+		return nil
+	}
+	lat, err := strconv.ParseFloat(latStr, 64)
+	if err != nil {
+		log.Printf("Invalid latitude: %v", err)
+		return nil
+	}
+	lon, err := strconv.ParseFloat(lonStr, 64)
+	if err != nil {
+		log.Printf("Invalid longitude: %v", err)
+		return nil
+	}
+	var apps []int
+	for _, app := range strings.Split(rawApps, ",") {
+		appID, err := strconv.Atoi(strings.TrimSpace(app))
+		if err != nil {
+			log.Printf("Invalid app ID: %v", err)
+			continue
+		}
+		apps = append(apps, appID)
+	}
+	return &AppsInstalled{
+		DevType: devType,
+		DevID:   devID,
+		Lat:     lat,
+		Lon:     lon,
+		Apps:    apps,
+	}
+}
+
+func dotRename(filePath string) {
+	dir, file := filepath.Split(filePath)
+	if err := os.Rename(filePath, filepath.Join(dir, "."+file)); err != nil {
+		log.Printf("Error renaming file: %v", err)
+	}
+}
+
+func insertAppsinstalled(memcAddr string, ai AppsInstalled, dryRun bool) bool {
+	ua := &appsinstalled.UserApps{
+		Lat:  ai.Lat,
+		Lon:  ai.Lon,
+		Apps: ai.Apps,
+	}
+	key := fmt.Sprintf("%s:%s", ai.DevType, ai.DevID)
+	packed, err := proto.Marshal(ua)
+	if err != nil {
+		log.Printf("Cannot marshal protobuf: %v", err)
+		return false
+	}
+	data := map[string][]byte{key: packed}
+	memc := NewMemcacheStorage(memcAddr)
+	if dryRun {
+		log.Printf("%s - %s -> %s", memcAddr, key, ua)
+	} else {
+		err := memc.Client.Set(data)
+		if err != nil {
+			log.Printf("Cannot write to memc %s: %v", memcAddr, err)
+			return false
+		}
+	}
+	return true
 }
